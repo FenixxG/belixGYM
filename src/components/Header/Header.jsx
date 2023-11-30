8cce58fd595f7056917eed2935323ad481376819
Cristian Miranda <cramirandav@gmail.com>
2023-11-29 20:06:01 -0600

    Modificacion de Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,21 @@
 import menus from './menu.json';
 import './header.css';
+import Logo from '../../assets/belixlogo.png';
 
 const Header = () => {
     const menuItems = menus.map((menu) => {
+
+        const subMenu = menu.submenu? menu.submenu.map((menusub) => {
+            <li key={menusub.id}>
+                <a href={menusub.link}>{menusub.nombre}</a>
+            </li>
+        }):null;
+
         return (
             <li key={menu.id}>
                 <a href={menu.link}>{menu.nombre}</a>
-                {menu.submenu && (
-                    <ul className="dropdownMenu">
-                        
-                    </ul>
+                {subMenu && (
+                    <ul>{subMenu}</ul>
                 )}
             </li>
         );
@@ -18,51 +24,11 @@ const Header = () => {
     return (
         <header>
             <div className="logo">
-                <img src="" alt="Logo" />
+                <img src={Logo} alt="Logo" />
                 <h1>Belix Total Fitness</h1>
             </div>
             <nav>
                 <ul className="navLinks">
-                    {/* <li>
-                        <a href>Inicio</a>
-                    </li>
-                    <li>
-                        <a href>Que ofrecemos</a>
-                        <ul className="dropdownMenu">
-                            <li>
-                                <a href>Servicios</a>
-                            </li>
-                            <li>
-                                <a href>Mensualidades</a>
-                            </li>
-                            <li>
-                                <a href>Productos</a>
-                                <ul className="dropdownMenu">
-                                    <li>
-                                        <a href>Consumibles</a>
-                                    </li>
-                                    <li>
-                                        <a href>Accesorios</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href>Programas</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href>Blog</a>
-                    </li>
-                    <li>
-                        <a href>Contacto</a>
-                    </li>
-                    <li>
-                        <a href>Acerca de</a>
-                    </li>
-                    <li>
-                        <a href>FAQ</a>
-                    </li> */}
                     {menuItems}
                 </ul>
             </nav>
