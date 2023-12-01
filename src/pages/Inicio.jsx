8ea5e9624c36fd498ba217deba912566c1ffb400
Cristian Miranda <cramirandav@gmail.com>
2023-12-01 17:32:38 -0600

    Actualizacion Footer

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -7,21 +7,20 @@ import imagenHero from '../assets/imagenHero.jpg';
 export const Inicio = () => {
     return (
         <Page>
-            <section className="hero">
+            {/* <section className="hero">
                 <img src={imagenHero} alt="Hero" />
             </section>
 
             <section className="about">
                 <div className="about-content">
                     <img src="ruta_de_tu_imagen" alt="Acerca de" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores, est praesentium possimus iusto facere repellat sit doloribus asperiores qui quidem reiciendis mollitia neque at corrupti. Quia quasi eveniet placeat!</p>
+                    <p>lorem</p>
                 </div>
             </section>
 
             <section className="location">
                 <div className="location-content">
                     <p>Aquí va tu dirección</p>
-                    {/* El iframe del mapa de Google Maps */}
                     <iframe
                         title="Mapa"
                         src="https://www.google.com/maps/embed?..."
@@ -31,7 +30,7 @@ export const Inicio = () => {
                         loading="lazy"
                     ></iframe>
                 </div>
-            </section>
+            </section> */}
         </Page>
     );
 };
