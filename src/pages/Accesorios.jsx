4daa4d5687e0e1c57a860dd6f75faffed05892c5
Archaga0 <gabrielarchaga2003@gmail.com>
2023-12-02 20:36:25 -0600

    Agregando Pagina de Accesorios

--- src/pages/Accesorios.jsx
+++ src/pages/Accesorios.jsx
@@ -1,11 +1,23 @@
 import Page from "./Page";
+import './accesorios.css';
+import { CardGallery } from "../components/Accesorios/GalleryAccesorio";
+
+import cardData from '../data/accesorios.json';
 
 export const Accesorios = () => {
-    return (
-      <Page>
-        <section>
-          <h2 className="m-4 p-4 bg-blue-400 text-red-400">Pagina Accesorios</h2>
-        </section>
-      </Page>
-    )
+  return (
+    <Page>
+      <section>
+        <div class="contenedor-accesorios">
+          <div>
+            <div class="titulos">
+              <h1>Productos</h1>
+              <h2>Contamos con los Accesorios de la mayor calidad</h2>
+            </div>
+            <CardGallery cards={cardData} />
+          </div>
+        </div>
+      </section>
+    </Page>
+  )
 }
