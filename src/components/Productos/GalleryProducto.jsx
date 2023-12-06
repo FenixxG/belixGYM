4017d16889bf3344c23844e9425a616a87108297
Cristian Miranda <cramirandav@gmail.com>
2023-12-05 21:12:26 -0600

    Actualizacion de estilos a Tailwind

--- src/components/Productos/GalleryProducto.jsx
+++ src/components/Productos/GalleryProducto.jsx
@@ -1,29 +1,9 @@
 import { Card } from "./CardProducto";
-<<<<<<< HEAD
-import './galleryproducto.css';
-export const CardGallery = ({ cards }) => {
-    // const onClickHandler = (nombre) => {
-    //     alert(nombre);
-    // }
-
-    const cardsList = cards.map((card) => {
-        return (
-            <Card
-                key={card.id}
-                nombre={card.nombre}
-                descripcion={card.descripcion}
-                image={card.imgUrl}
-                precio={card.precio}
-            />
-        );
-    });
-=======
 import "./galleryproducto.css";
 export const CardGallery = ({ cards }) => {
   const onClickHandler = (nombre) => {
     alert(nombre);
   };
->>>>>>> 107316a9a958961644d11ff2211560f9d7d7154a
 
   const cardsList = cards.map((card) => {
     return (
